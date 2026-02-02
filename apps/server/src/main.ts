2a5c1c08fd8a8197740697847300eadfb2fd0da2
Rodrigo Rocha <rodrocha444@gmail.com>
2026-02-02 18:22:09 -0300

    🔧 Add enableCors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,6 +7,12 @@ async function bootstrap() {
     logger: new SimpleTimestampLogger(),
   });
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
